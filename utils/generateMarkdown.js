a4b10ab0cbb4ea4e0d64618f089a6178fe0c2bb9
D <Dickerson.Morgan@outlook.com>
2022-12-03 18:38:40 -0500

    added links to license badge and link sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,13 +3,13 @@
 function renderLicenseBadge(license) {
   switch (license) {
     case "MIT":
-      return "[MIT license](https://img.shields.io/npm/l/np?style=flat-square)";
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
     case "Apache 2.0":
-      return "Apache 2.0 license";
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]";
     case "GPL 3.0":
-      return "GPL 3.0 license";
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
     case "BSD 3":
-      return "BSD 3 license";
+      return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]";
     case "None":
       return "";
   }
@@ -20,13 +20,13 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   switch (license) {
     case "MIT":
-      return "MIT license";
+      return "(https://opensource.org/licenses/MIT)";
     case "Apache 2.0":
-      return "Apache 2.0 license";
+      return "(https://opensource.org/licenses/Apache-2.0)";
     case "GPL 3.0":
-      return "GPL 3.0 license";
+      return "(https://www.gnu.org/licenses/gpl-3.0)";
     case "BSD 3":
-      return "BSD 3 license";
+      return "(https://opensource.org/licenses/BSD-3-Clause)";
     case "None":
       return "";
   }
